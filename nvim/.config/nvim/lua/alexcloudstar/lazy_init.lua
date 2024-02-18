37f9eb559841a41ce258e2501cbdd2607065e332
alexcloudstar <alexcloudstar@gmail.com>
2024-02-18 17:00:22 +0200

    fix: Packages & cleaner way to manage them

--- nvim/.config/nvim/lua/alexcloudstar/lazy_init.lua
+++ nvim/.config/nvim/lua/alexcloudstar/lazy_init.lua
@@ -13,46 +13,9 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    -- Packer can manage itself
-    {'wbthomason/packer.nvim'},
-    {
-        'nvim-telescope/telescope.nvim', tag = '0.1.5',
-        -- or                            , branch = '0.1.x',
-        requires = { {'nvim-lua/plenary.nvim'} }
-    },
-    {
-        'catppuccin/nvim',
-        as = 'catppuccin',
-        config = function()
-            vim.cmd('colorscheme catppuccin')
-        end
-    },
-    {'nvim-treesitter/nvim-treesitter', run =  ":TSUpdate"},
-    {"theprimeagen/harpoon"},
-    {"mbbill/undotree"},
-    {"tpope/vim-fugitive"},
-    {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v1.x',
-        requires = {
-            -- LSP Support
-            {'neovim/nvim-lspconfig'},
-            {'williamboman/mason.nvim'},
-            {'williamboman/mason-lspconfig.nvim'},
-
-            -- Autocompletion
-            {'hrsh7th/nvim-cmp'},
-            {'hrsh7th/cmp-buffer'},
-            {'hrsh7th/cmp-path'},
-            {'saadparwaiz1/cmp_luasnip'},
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'hrsh7th/cmp-nvim-lua'},
-
-            -- Snippets
-            {'L3MON4D3/LuaSnip'},
-            {'rafamadriz/friendly-snippets'},
-        }
-    },
-    {"github/copilot.vim"},
+    require("lazy").setup({
+        spec = "alexcloudstar.lazy",
+        change_detection = { notify = false }
+    })
 })
 
