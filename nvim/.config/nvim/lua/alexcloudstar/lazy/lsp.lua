d77f669eef26a4212a430888cd226d219973e62e
alexcloudstar <alexcloudstar@gmail.com>
2024-05-09 20:47:12 +0300

    feat: GO LSP

--- nvim/.config/nvim/lua/alexcloudstar/lazy/lsp.lua
+++ nvim/.config/nvim/lua/alexcloudstar/lazy/lsp.lua
@@ -29,11 +29,11 @@ return {
         require("mason-lspconfig").setup({
             ensure_installed = {
                 "lua_ls",
-                "rust_analyzer",
                 "tsserver",
                 "eslint",
                 "html",
                 "tailwindcss",
+                "gopls",
             },
             handlers = {
                 function(server_name) -- default handler (optional)
