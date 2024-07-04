0804e097efc5ab34e70aa79ca16a625462c4e443
alexcloudstar <alexcloudstar@gmail.com>
2024-07-04 09:59:18 +0300

    update: Nvim config

--- nvim/.config/nvim/lua/alexcloudstar/lazy_init.lua
+++ nvim/.config/nvim/lua/alexcloudstar/lazy_init.lua
@@ -6,16 +6,14 @@ if not vim.loop.fs_stat(lazypath) then
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    "--branch=stable",
     lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    require("lazy").setup({
-        spec = "alexcloudstar.lazy",
-        change_detection = { notify = false }
-    })
+    spec = "alexcloudstar.lazy",
+    change_detection = { notify = false }
 })
 
