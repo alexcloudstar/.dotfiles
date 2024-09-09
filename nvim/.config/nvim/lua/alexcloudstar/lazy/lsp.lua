e29803eec522342a22285593583d369ae51fd08c
alexcloudstar <alexcloudstar@gmail.com>
2024-09-09 11:13:36 +0300

    fix: rename tsserver to ts_ls

--- nvim/.config/nvim/lua/alexcloudstar/lazy/lsp.lua
+++ nvim/.config/nvim/lua/alexcloudstar/lazy/lsp.lua
@@ -29,7 +29,7 @@ return {
         require("mason-lspconfig").setup({
             ensure_installed = {
                 "lua_ls",
-                "tsserver",
+                "ts_ls",
                 "eslint",
                 "html",
                 "tailwindcss",
